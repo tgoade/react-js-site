f26aceea7e0b572da9b6d30d7d5f7e904e08eca7
tmdigitalspace <88467417+tmdigitalspace@users.noreply.github.com>
2021-11-03 08:22:46 -0400

    added nav bar tests

--- src/components/nav/NavBar.test.js
+++ src/components/nav/NavBar.test.js
@@ -1,11 +1,38 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from "react-router-dom";
 import NavBar from './NavBar';
 
 describe('Test suit for navigation components', () => {
-    test('Check if text appears on screen', () => {
+    test('checks if text appears on screen', () => {
+        // Arrange
         render(<BrowserRouter><NavBar /></BrowserRouter>);
+        // Act
+        // (nothing)
+        // Assert
         const navElement = screen.getByText(/places to stay/i);
         expect(navElement).toBeInTheDocument();
+    });
+
+    test('checks if "Check In" appears if "Places to Stay" button is clicked', () => {
+        // Arrange
+        render(<BrowserRouter><NavBar /></BrowserRouter>);
+        // Act
+        const placesElememt = screen.getByText(/places to stay/i);
+        userEvent.click(placesElememt);
+        // Assert
+        const placesOutput = screen.getByText(/check in/i);
+        expect(placesOutput).toBeInTheDocument();
+    });
+
+    test('checks if "Guests" appears if "Experiences" button is clicked', () => {
+        // Arrange
+        render(<BrowserRouter><NavBar /></BrowserRouter>);
+        // Act
+        const experiencesElememt = screen.getByText(/experiences/i);
+        userEvent.click(experiencesElememt);
+        // Assert
+        const experiencesOutput = screen.getByText(/guests/i);
+        expect(experiencesOutput).toBeInTheDocument();
     });
 });
